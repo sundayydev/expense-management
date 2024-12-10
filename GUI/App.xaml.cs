e3572e411fb0a80245e669db71556e646d6da06b
Ngô Mạnh Hùng <sundayy.dev@gmail.com>
2024-12-10 15:55:57 +0700

    Create UcRemind and custom style calendar

--- GUI/App.xaml.cs
+++ GUI/App.xaml.cs
@@ -1,9 +1,22 @@
-﻿namespace GUI
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+
+namespace GUI
 {
    /// <summary>
    /// Interaction logic for App.xaml
    /// </summary>
    public partial class App
    {
-   }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Đặt ngôn ngữ mặc định là tiếng Việt
+            CultureInfo culture = new CultureInfo("vi-VN");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+    }
 }
