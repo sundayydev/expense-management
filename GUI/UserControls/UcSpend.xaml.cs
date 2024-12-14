e2f75a6f39df89ab41e710e1fd57820440ac899f
Elihoang <129831728+Elihoang@users.noreply.github.com>
2024-12-14 17:36:48 +0700

    êxpense

--- GUI/UserControls/UcSpend.xaml.cs
+++ GUI/UserControls/UcSpend.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using GUI.View;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -18,10 +19,10 @@ namespace GUI.UserControls
         {
             InitializeComponent();
             LoadData();
-            InvoiceDataGrid.ItemsSource = Expenses;
+            dvgExpense.ItemsSource = Expenses;
         }
 
-        // Load initial data
+       
         private void LoadData()
         {
             Expenses = new ObservableCollection<Expense>
@@ -34,7 +35,7 @@ namespace GUI.UserControls
             };
         }
 
-        // TextChanged event for searching
+      
         private void txtFind_TextChanged(object sender, TextChangedEventArgs e)
         {
             string searchText = txtFind.Text.ToLower();
@@ -44,29 +45,23 @@ namespace GUI.UserControls
                 expense.CategoryID.ToString().Equals(searchText)
             ).ToList();
 
-            InvoiceDataGrid.ItemsSource = new ObservableCollection<Expense>(res);
+            dvgExpense.ItemsSource = new ObservableCollection<Expense>(res);
         }
 
-        // Add new expense
+     
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            var newExpense = new Expense
-            {
-                ExpenseID = Expenses.Count + 1,
-                UserID = 106,
-                CategoryID = 2,
-                RecipientID = 206,
-                Amount = 250m,
-                ExpenseDate = DateTime.Now.Date,
-                Note = "New Item",
-                CreatedAt = DateTime.Now
-            };
+            WFormExpense formExpense = new WFormExpense();
+            formExpense.OnExpenseAdded += FormExpense_OnExpenseAdded; 
+            formExpense.ShowDialog();
 
+        }
+        private void FormExpense_OnExpenseAdded(Expense newExpense)
+        {
             Expenses.Add(newExpense);
-            InvoiceDataGrid.ItemsSource = new ObservableCollection<Expense>(Expenses);
+            dvgExpense.ItemsSource = new ObservableCollection<Expense>(Expenses); 
         }
 
-        // Delete an expense
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
@@ -74,11 +69,11 @@ namespace GUI.UserControls
            
             if (expenseToDelete != null)
             {
-                DialogCustoms dialog = new DialogCustoms("Bạn có muốn xoa ko ?", "Thông báo", DialogCustoms.YesNo);
+                DialogCustoms dialog = new DialogCustoms("Bạn có muốn xoá ko ?", "Thông báo", DialogCustoms.YesNo);
                 if (dialog.ShowDialog() == true)
                 {
                     Expenses.Remove(expenseToDelete);
-                    InvoiceDataGrid.ItemsSource = new ObservableCollection<Expense>(Expenses);
+                    dvgExpense.ItemsSource = new ObservableCollection<Expense>(Expenses);
                 }        
             }
         }
--- GUI/View/WFormExpense.xaml.cs
+++ GUI/View/WFormExpense.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GUI.UserControls;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace GUI.View
 {
@@ -19,9 +10,59 @@ namespace GUI.View
     /// </summary>
     public partial class WFormExpense : Window
     {
+        public Expense ExpenseData { get; set; }
+        public event Action<Expense> OnExpenseAdded;
+
         public WFormExpense()
         {
             InitializeComponent();
+            ExpenseData = new Expense();
+            this.DataContext = this;
+        }
+
+        private void btnSaveAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Expense newExpense = new Expense
+            {
+                /*ExpenseID= 11,
+                UserID = 1,
+                CategoryID = 1,
+                RecipientID = 1,
+                Amount = 120000  ,
+                ExpenseDate = DateTime.Now,
+                Note = "co cao con c",
+                CreatedAt = DateTime.Now*/
+                ExpenseID = 11,
+                UserID = int.Parse(txtUser.Text.ToString()),
+                CategoryID = int.Parse(txtCatagory.Text.ToString()),
+                RecipientID = int.Parse(txtRecipient.Text.ToString()),
+                Amount = decimal.Parse(txtAmount.Text.ToString()),
+                ExpenseDate = DateTime.Now,
+                Note = new TextRange(rtbNote.Document.ContentStart, rtbNote.Document.ContentEnd).Text.Trim(),
+                CreatedAt = DateTime.Now
+            };
+
+            OnExpenseAdded?.Invoke(newExpense);
+
+            DialogCustoms dialog = new DialogCustoms("Bạn có muốn cập nhật dữ liệu không?", "Thông báo", DialogCustoms.YesNo);
+            if (dialog.ShowDialog() == true)
+            {
+                DialogCustoms successDialog = new DialogCustoms("Cập nhật thành công", "Thông báo", DialogCustoms.Show);
+                successDialog.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                DialogCustoms cancelDialog = new DialogCustoms("Hủy thao tác", "Thông báo", DialogCustoms.OK);
+                cancelDialog.ShowDialog();
+                this.Close();
+            }
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogCustoms cancelDialog = new DialogCustoms("Hủy thao tác", "Thông báo", DialogCustoms.Show);
+            cancelDialog.ShowDialog();
+            this.Close(); 
         }
     }
 }
