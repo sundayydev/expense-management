3de9a23f077bd8eec46f32bbf841a9ccd8e756dd
Ngô Mạnh Hùng <sundayy.dev@gmail.com>
2024-12-21 12:59:32 +0700

    Update LoginDto.cs

--- BLL/DTO/User/LoginDto.cs
+++ BLL/DTO/User/LoginDto.cs
@@ -1,8 +1,14 @@
-﻿namespace BLL.DTO.User
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO.User
 {
-   public class LoginDto
-   {
-      public string Email { get; set; }
-      public string Password { get; set; }
-   }
+    public class LoginDTO
+    {
+        public string Email {  get; set; }
+        public string Password { get; set; }
+    }
 }
