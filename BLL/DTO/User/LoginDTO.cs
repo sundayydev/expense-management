6461aea421c3a8d7ebaae7c72b08b78056c777bc
Elihoang <129831728+Elihoang@users.noreply.github.com>
2024-12-23 11:37:31 +0700

    Update LoginDTO.cs

--- BLL/DTO/User/LoginDTO.cs
+++ BLL/DTO/User/LoginDTO.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BLL.DTO.User
+﻿namespace BLL.DTO.User
 {
-    public class LoginDTO
-    {
-        public string Email {  get; set; }
-        public string Password { get; set; }
-    }
+   public class LoginDto
+   {
+      public string Email { get; set; }
+      public string Password { get; set; }
+   }
 }
