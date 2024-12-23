d1d8269d6d792455a250ce348bacf340e592a8f1
Elihoang <129831728+Elihoang@users.noreply.github.com>
2024-12-23 17:01:35 +0700

    44
    Delete Add expense

---
+++ BLL/DTO/Expenses/ExpenseDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO.Expenses
+{
+    public class ExpenseDto
+    {
+        public string ExpenseId { get; set; }
+
+        public string UserId { get; set; }
+        public string CategoryId { get; set; }
+        public string RecipientId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ExpenseDate { get; set; }
+        public string Note { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
